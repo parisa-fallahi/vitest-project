bfe54d4ce1635ef8a4c4598799b4fae826271bb3
parydev <parisafallahi71@gmail.com>
2024-06-28 12:01:54 +0330

    tests for userlist example done successfully

--- vitest-project/src/app/app.component.spec.ts
+++ vitest-project/src/app/app.component.spec.ts
@@ -1,29 +1,82 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+// import { TestBed } from '@angular/core/testing';
+// import { AppComponent } from './app.component';
+
+// describe('AppComponent', () => {
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [AppComponent],
+//     }).compileComponents();
+//   });
+
+//   it('should create the app', () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     const app = fixture.componentInstance;
+//     expect(app).toBeTruthy();
+//   });
+
+//   it(`should have the 'vitest-project' title`, () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     const app = fixture.componentInstance;
+//     expect(app.title).toEqual('vitest-project');
+//   });
+
+//   it('should render title', () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     fixture.detectChanges();
+//     const compiled = fixture.nativeElement as HTMLElement;
+//     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, vitest-project');
+//   });
+// });
+import { it, expect, describe } from 'vitest'
+import {render,screen} from '@testing-library/angular'
+import '@testing-library/jest-dom'
+import '@testing-library/jest-dom/vitest'
+import { AppComponent } from './app.component'
+import { i } from 'vitest/dist/reporters-yx5ZTtEV.js'
+import { User } from '../entities'
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+  it('should render h1 element', async() => {
+    await render(AppComponent, {
+      componentProperties: {title: 'parisa'},
+      
+    })
+    const heading = screen.getByRole('heading');
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/hello/i);
+   })
+})
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
 
-  it(`should have the 'vitest-project' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('vitest-project');
-  });
+describe('UserList', () => {
+  it('should rerun no user found if giver empty array', async() => {
+    await render(AppComponent,{
+      componentProperties:{userList:[]},
+    })
+    expect(screen.getByText(/no users/i)).toBeInTheDocument();
+    
+  }),
+  it('should return array of users if the array is not empty', async() => {
+    const users: User[]=[{
+      id:11, name:'test-name', isAdmin:false
+    },
+    {
+      id:12, name:'maryam', isAdmin:false
+    }]
+    await render(AppComponent,{
+      componentProperties:{userList:users},
+    })
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, vitest-project');
-  });
-});
+    users.forEach(user => {
+      //const link = screen.getByRole('link', {name:user.name});
+      // const link =screen.getAllByRole('listitem').find(listitem => listitem.textContent === user.name)
+      // expect(link).toBeInTheDocument();
+      expect(screen.getByRole('link', { name: user.name })).toBeInTheDocument();
+      expect(screen.getByRole('link', { name: user.name })).toHaveAttribute('href', 'https://www.test.com/')
+    }
+     
+      )
+    
+  })
+  
+})
--- vitest-project/src/app/app.component.ts
+++ vitest-project/src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { User } from '../entities';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule, HttpClientModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'vitest-project';
+export class AppComponent implements OnInit {
+userList:User[] =[{
+  id:0,
+  name:'parisa',
+  isAdmin:false
+},{
+  id:1,
+  name:'ali',
+  isAdmin:false
+},
+{
+id:2,
+name:'reza',
+isAdmin:true
+}];
+
+  title : string = 'aaa';
+
+  ngOnInit():void{
+    this.getUserList();
+    
+  }
+
+  getUserList(){
+    //return this.userList;
+    console.log(this.userList)
+  }
 }
